a18edc0581fc0bd07375e4bf6b1281abd86d2fcb
Luke Davids <lukedavids@gmail.com>
2025-04-02 10:30:37 -0500

    Stretch Goal #1 Completed

--- src/scripts/guestHTML.js
+++ src/scripts/guestHTML.js
@@ -2,16 +2,20 @@ import { getGuests, getAreas } from "./Database.js"
 
 export const guestHTML = () => {
 const guests = getGuests();
+const areas = getAreas()
 let guestListHTML = `
     <h2>Current Park Guests</h2>
     <ol>`;
 
 for (const guest of guests) {
+    const guestLocation = areas.filter(obj => obj.id === guest.areaId).map(obj => obj.area)
     guestListHTML += `
             <li data-type="guest"
                 data-guest-id="${guest.id}"
-            >${guest.firstName} ${guest.lastName}
+                data-area="${guest.areaId}"
+            >${guest.firstName} ${guest.lastName} (${guestLocation.join('')})
             </li>`;
+        
 }
 
 guestListHTML += "</ol>";
